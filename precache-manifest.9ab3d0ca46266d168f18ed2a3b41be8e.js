fb3d75a060e3ab4303d09679d3d28b56edc99655
atul <atul@tablecheck.com>
2021-04-29 16:05:55 +0900

    Updates

--- precache-manifest.9ab3d0ca46266d168f18ed2a3b41be8e.js
+++ precache-manifest.9ab3d0ca46266d168f18ed2a3b41be8e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "435f396fc6a0df47bd1d9bbe020d724f",
+    "revision": "47a8ec048d27f6f0e844ca4419d5b109",
     "url": "/minim/index.html"
   },
   {
-    "revision": "2e6edc76d8a54399210d",
-    "url": "/minim/static/css/2.1bd11376.chunk.css"
+    "revision": "f463a71106868c31c439",
+    "url": "/minim/static/css/2.96479aa2.chunk.css"
   },
   {
-    "revision": "3c770593e7de46c7de13",
-    "url": "/minim/static/css/main.e1c1c611.chunk.css"
+    "revision": "749fb8365cf0b2537fad",
+    "url": "/minim/static/css/main.e711c2c1.chunk.css"
   },
   {
-    "revision": "2e6edc76d8a54399210d",
-    "url": "/minim/static/js/2.af68c281.chunk.js"
+    "revision": "f463a71106868c31c439",
+    "url": "/minim/static/js/2.dd66b212.chunk.js"
   },
   {
     "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
-    "url": "/minim/static/js/2.af68c281.chunk.js.LICENSE"
+    "url": "/minim/static/js/2.dd66b212.chunk.js.LICENSE"
   },
   {
-    "revision": "3c770593e7de46c7de13",
-    "url": "/minim/static/js/main.edc38a29.chunk.js"
+    "revision": "749fb8365cf0b2537fad",
+    "url": "/minim/static/js/main.6c21662e.chunk.js"
   },
   {
     "revision": "1fbd6f56df0883dc0ea0",
